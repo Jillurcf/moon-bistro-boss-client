479487541e508ecabdd6cb47b8bfa7500034d9a1
jillurcf <jrtushar@gmail.comm>
2024-03-27 08:15:51 -0700

    Home component created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,17 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLaout from "../Layout/MainLaout";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLaout></MainLaout>,
+      children: [
+        {
+            path: '/',
+        }
+      ]
     },
   ]);
   export default router;
