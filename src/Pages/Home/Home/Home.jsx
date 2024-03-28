3954f6ab9464d254b28261813a4f3877fa60efd9
jillurcf <jrtushar@gmail.comm>
2024-03-28 08:33:59 -0700

    categories component created

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import Categories from "../Category/Categories";
 
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
           
             <Banner></Banner>
+            <Categories></Categories>
         </div>
     );
 };
