c4bdd87a5fe936360365a5a590e33e56d20ef680
jillurcf <jrtushar@gmail.comm>
2024-03-29 01:45:06 -0700

    helmet applied on menu page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+               <title>Bistro-menu</title>
+            </Helmet>
             <h2>Menu</h2>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
  <div className='max-w-screen-xl mx-auto'>
  <RouterProvider router={router} />
  </div>
   </React.StrictMode>,
+  </HelmetProvider>
 )
